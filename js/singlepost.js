478f838c48dad4b0aa84a799d1bc188f694485e0
Diluka Wittahachchi <diluka.999@gmail.com>
2013-10-03 18:12:55 +0530

    Initial commit

---
+++ js/singlepost.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('.blogpost-content').mCustomScrollbar({
+        mouseWheelPixels: 300,
+        theme: 'dark',
+        scrollButtons: {
+            enable: true
+        },
+        advanced: {
+            autoExpandHorizontalScroll: true
+        }
+    });
+});
